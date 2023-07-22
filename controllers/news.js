8856aeb636a83eb3599fe529ed2026001e120e2d
Mufiddin <mufiddin@softwareseni.com>
2023-07-22 16:42:42 +0700

    add blog detail and fix link

--- controllers/news.js
+++ controllers/news.js
@@ -4,7 +4,8 @@ const posts = require('../model/posts')
 
 class NewsController {
 
-    init(){
+    init() {
+        router.get('/:id', this.getDetail());
         router.get('/', this.get());
         return router;
     }
@@ -15,15 +16,36 @@ class NewsController {
             try {
                 let filtered = posts
                 const q = req.query.q || '';
-                if(q !== null || q !='' ){
+                if (q !== null || q != '') {
                     const regex = new RegExp(q, 'i');
                     filtered = posts.filter((item) => regex.test(item.title));
                 }
-                
-                const notfound = filtered.length > 0 ? false : true; 
-                res.render('news', {posts: filtered, q: q, notfound: notfound});
+
+                const notfound = filtered.length > 0 ? false : true;
+                res.render('news', { posts: filtered, q: q, notfound: notfound });
             } catch (error) {
                 console.error(error)
+            }
+        }
+    }
+
+    getDetail() {
+        return (req, res, next) => {
+            try {
+                let filtered = posts
+                const id = req.params.id || '';
+                if (id !== null || id != '') {
+                    const regex = new RegExp(id, 'i');
+                    filtered = posts.filter((item) => regex.test(item.id));
+                }
+
+                const notfound = filtered.length > 0 ? false : true;
+                const data = { posts: filtered, id: id, notfound: notfound }
+                console.log(data)
+                // res.send('lala')
+                res.render('news-detail', { posts: filtered, id: id, notfound: notfound });
+            } catch (error) {
+                console.error(error);
             }
         }
     }
