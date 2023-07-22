7e13e6dafe9cca0515a0f55a36a8c3f8121624bb
Mufiddin <mufiddin@softwareseni.com>
2023-07-22 21:57:30 +0700

    add cache 1 year

--- app.js
+++ app.js
@@ -14,7 +14,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'), {
+  maxAge: 31536000
+}));
 
 app = new CustomRouter(app).init();
 
